3a15bfc1bacb491b9f810bd51f05ba649dd4376b
owenmiz <owenmiz@students.cs.ubc.ca>
2023-04-10 16:31:11 -0700

    added .

--- prove.pl
+++ prove.pl
@@ -9,9 +9,13 @@ prove(Regex, String, loose, R) :- regex_parse(Regex, Tree), dfa_parse(Tree), pro
 prove_strong(Str, R) :- start(S), atom_chars(Str, C), prove_move_strong(C, S, Q), atom_chars(R, Q).
 prove_move_strong([H|[]], S, [H]) :- transition(S, H, S1), accepting(S1).
 prove_move_strong([H|T], S, [H|R]) :- transition(S, H, S1), prove_move_strong(T, S1, R).
+prove_move_strong([H|[]], S, [H]) :- transition(S, ., S1), accepting(S1).
+prove_move_strong([H|T], S, [H|R]) :- transition(S, ., S1), prove_move_strong(T, S1, R).
 
 %   Loose proof code, walks through the state machine using the string, and if an accepting state is reached returns true.
 prove_loose(Str, R) :- start(S), atom_chars(Str, C), prove_move_loose(C, S, Q), atom_chars(R, Q).
 prove_move_loose([H|_], S, [H]) :- transition(S, H, S1), accepting(S1).
 prove_move_loose([H|T], S, [H|R]) :- transition(S, H, S1), prove_move_loose(T, S1, R).
+prove_move_loose([H|_], S, [H]) :- transition(S, ., S1), accepting(S1).
+prove_move_loose([H|T], S, [H|R]) :- transition(S, ., S1), prove_move_loose(T, S1, R).
 prove_move_loose([_|T], S, R) :- start(S), prove_move_loose(T, S, R).
