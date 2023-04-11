b546d31bf1a340a4f5daba82429cc2acfc735c81
Floria Gu <gfzting@gmail.com>
2023-04-10 22:05:36 -0700

    Minor fixes to proof code, and add \d, \w, \s character classes

--- parse.pl
+++ parse.pl
@@ -1,51 +1,57 @@
 % regex_parse(Str, Tree)    is true if Str is a valid regex and its syntax tree is Tree.
 regex_parse(Str, Tree) :-
     string_chars(Str, Chars),
-    pattern(Chars, [], Tree).
+    re_pattern(Chars, [], Tree).
 
 % Pattern: top-level regex
-pattern(L0, L1, T) :- dis(L0, L1, T).
+re_pattern(L0, L1, T) :- re_dis(L0, L1, T).
 
 % Disjunction
 % dis nodes have 2 children
-dis(L0, L1, C) :- alt(L0, L1, C).
-dis(L0, L3, node(dis, C1, C2)) :-
-    alt(L0, L1, C1),
-    bar(L1, L2),
-    dis(L2, L3, C2).
+re_dis(L0, L1, C) :- re_alt(L0, L1, C).
+re_dis(L0, L3, node(dis, C1, C2)) :-
+    re_alt(L0, L1, C1),
+    re_bar(L1, L2),
+    re_dis(L2, L3, C2).
 
 % Alternative
 % alt nodes have 2 children
-alt(L0, L1, T) :- term(L0, L1, T).
-alt(L0, L2, node(alt, T, C)) :-
-    term(L0, L1, T),
-    alt(L1, L2, C).
+re_alt(L0, L1, T) :- re_term(L0, L1, T).
+re_alt(L0, L2, node(alt, T, C)) :-
+    re_term(L0, L1, T),
+    re_alt(L1, L2, C).
 
 % Term
-term(L0, L1, A) :- atom(L0, L1, A).
-term(L0, L1, Q) :- quant(L0, L1, Q).
+re_term(L0, L1, A) :- re_atom(L0, L1, A).
+re_term(L0, L1, Q) :- re_quant(L0, L1, Q).
 
 % Atom
-atom(['.' | L], L, node('.', empty, empty)).
-atom([C | L], L, node(C, empty, empty)) :- pattern_char(C). 
-atom(L0, L3, D) :-
-    lparen(L0, L1),
-    dis(L1, L2, D),
-    rparen(L2, L3).
+re_atom(['.' | L], L, node('.', empty, empty)).
+re_atom([C | L], L, node(C, empty, empty)) :- re_pattern_char(C). 
+re_atom([\, 'd' | L], L, node('\\d', empty, empty)).
+re_atom([\, 'D' | L], L, node('\\D', empty, empty)).
+re_atom([\, 's' | L], L, node('\\s', empty, empty)).
+re_atom([\, 's' | L], L, node('\\S', empty, empty)).
+re_atom([\, 'w' | L], L, node('\\w', empty, empty)).
+re_atom([\, 'W' | L], L, node('\\W', empty, empty)).
+re_atom(L0, L3, D) :-
+    re_lparen(L0, L1),
+    re_dis(L1, L2, D),
+    re_rparen(L2, L3).
 
 % Quantifier (the symbol)
-quantifier(['*' | L], L, '*').
-quantifier(['+' | L], L, '+').
-quantifier(['?' | L], L, '?').
+re_quantifier(['*' | L], L, '*').
+re_quantifier(['+' | L], L, '+').
+re_quantifier(['?' | L], L, '?').
 
 % Quantifier (the expression type: atom + quantifier)
 % quant nodes have 2 children - the first is the quantifier type, the second is the element
-quant(L0, L2, node(Q, A, empty)) :-
-    atom(L0, L1, A),
-    quantifier(L1, L2, Q).
+re_quant(L0, L2, node(Q, A, empty)) :-
+    re_atom(L0, L1, A),
+    re_quantifier(L1, L2, Q).
 
 % PatternCharacter
-pattern_char(C) :- not_in(C, [
+re_pattern_char(C) :- not_in(C, [
     '^',
     '$',
     \,
@@ -63,9 +69,9 @@ pattern_char(C) :- not_in(C, [
 ]).
 
 % Special characters
-lparen(['(' | L], L).
-rparen([')' | L], L).
-bar(['|' | L], L).
+re_lparen(['(' | L], L).
+re_rparen([')' | L], L).
+re_bar(['|' | L], L).
 
 % not_in(E, L)      is true if E is not contained in L.
 not_in(_, []).
--- prove.pl
+++ prove.pl
@@ -2,24 +2,33 @@
 :- [parse].
 
 %   The two versions of prove that can be used.  Strong matches only the string to the regex, loose matches any substring as well.
+%   Note: won't work with retry.
 prove(Regex, String, strong, R) :- regex_parse(Regex, Tree), dfa_parse(Tree), prove_strong(String, R), clear.
 prove(Regex, String, loose, R) :- regex_parse(Regex, Tree), dfa_parse(Tree), prove_loose(String, R), clear.
 
 %   Some basic error handling for prove
-prove(Regex, _, _, _) :- \+ regex_parse(Regex, _), write("Invalid Regex").
-prove(_, _, Q, _) :- \+ Q = strong, \+ Q = loose, write("Please specify strong or loose").
+prove(Regex, _, _, _) :- \+ regex_parse(Regex, _), write("Invalid Regex"), fail.
+prove(_, _, Q, _) :- \+ Q = strong, \+ Q = loose, write("Please specify strong or loose"), fail.
 
 %   Strong proof code, walks through the state machine using the string and if the string ends at an accepting state returns true.
-prove_strong(Str, R) :- start(S), atom_chars(Str, C), prove_move_strong(C, S, Q), atom_chars(R, Q).
-prove_move_strong([H|[]], S, [H]) :- transition(S, H, S1), accepting(S1).
-prove_move_strong([H|T], S, [H|R]) :- transition(S, H, S1), prove_move_strong(T, S1, R).
-prove_move_strong([H|[]], S, [H]) :- transition(S, ., S1), accepting(S1).
-prove_move_strong([H|T], S, [H|R]) :- transition(S, ., S1), prove_move_strong(T, S1, R).
+prove_strong(Str, R) :- start(S), string_chars(Str, C), prove_move_strong(C, S, Q), string_chars(R, Q).
+prove_move_strong([H|[]], S, [H]) :- transition(S, C, S1), matches(C, H), accepting(S1).
+prove_move_strong([H|T], S, [H|R]) :- transition(S, C, S1), matches(C, H), prove_move_strong(T, S1, R).
 
 %   Loose proof code, walks through the state machine using the string, and if an accepting state is reached returns true.
-prove_loose(Str, R) :- start(S), atom_chars(Str, C), prove_move_loose(C, S, Q), atom_chars(R, Q).
-prove_move_loose([H|_], S, [H]) :- transition(S, H, S1), accepting(S1).
-prove_move_loose([H|T], S, [H|R]) :- transition(S, H, S1), prove_move_loose(T, S1, R).
-prove_move_loose([H|_], S, [H]) :- transition(S, ., S1), accepting(S1).
-prove_move_loose([H|T], S, [H|R]) :- transition(S, ., S1), prove_move_loose(T, S1, R).
+prove_loose(Str, R) :- start(S), string_chars(Str, C), prove_move_loose(C, S, Q), string_chars(R, Q).
+prove_move_loose([H|_], S, [H]) :- transition(S, C, S1), matches(C, H), accepting(S1).
+prove_move_loose([H|T], S, [H|R]) :- transition(S, C, S1), matches(C, H), prove_move_loose(T, S1, R).
 prove_move_loose([_|T], S, R) :- start(S), prove_move_loose(T, S, R).
+
+%   matches(Node, Char)     Is true if the character or character class Node matches Char.
+matches('.', _).
+matches('\\d', C) :- char_type(C, digit).
+matches('\\s', C) :- char_type(C, space).
+matches('\\w', C) :- char_type(C, alnum).
+matches('\\w', C) :- char_type(C, digit).
+matches('\\w', '_').
+matches('\\D', C) :- \+ matches('\\d', C).
+matches('\\S', C) :- \+ matches('\\S', C).
+matches('\\W', C) :- \+ matches('\\w', C).
+matches(C, C) :- not_in(C, ['\\d', '\\s', '\\w', '\\D', '\\S', '\\W', '.']).
